1a262a6285940fcaeaaef097bfde6cb50bbe94cb
d1sxrder3d <danikgamer228@inbox.ru>
2025-11-13 19:05:21 +0300

    fix: Удален случайно добавленный файл

--- 11.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta, timezone
-
-data1 = datetime.utcnow()
-
-data2 = datetime.utcnow() + timedelta(minutes=15)
-
-if __name__ == "__main__":
-    print(data1)
-    print(data2)
